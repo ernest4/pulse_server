8f490c2c40868469511387308f3cbc860a2898a2
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2020-10-22 21:23:05 +0100

    client progress

--- src/pulse/client.cr
+++ src/pulse/client.cr
@@ -1,11 +1,75 @@
+# TODO: speeecs ?!?!?
 module Pulse
   class Client
     def initialize(socket : HTTP::WebSocket, client_id)
       @socket = socket
+      @client_id = client_id
     end
 
-    def enter_room
-      # TODO: ...
+    def enter_room(maps)
+      # TODO: ...wip
+      # maps[...wip...].clients.each do |client| # TODO: make a broadcast method on Pulse::Map
+      #   @socket.send(Pulse::Message.build(["Enter", user.name, user.position_x, use.position_y]))
+      # end
+
+      # maps[...wip...].clients.push(self)
     end
   end
 end
+
+
+  # # Handle incoming message and dispatch it to all connected clients
+  # socket.on_message do |message|
+  #   # messages.push message
+  #   # sockets.each do |a_socket|
+  #   #   a_socket.send messages.to_json
+  #   # end
+
+    
+  #   puts "non binary"
+  #   puts message # TESTING...
+    
+  #   # TESTING: echo
+  #   socket.send(message)
+  # end
+
+  # socket.on_binary do |message|
+  #   # messages.push message
+  #   # sockets.each do |a_socket|
+  #   #   a_socket.send messages.to_json
+  #   # end
+
+    
+  #   puts "binary"
+  #   puts message # TESTING...
+  #   puts message.class # TESTING...
+
+  #   # Converting to message from Slice(UInt8) to Slice(UInt16)
+  #   uint16_slice = Slice(UInt16).new(message.to_unsafe.as(Pointer(UInt16)), message.size // 2)
+
+  #   puts "converted to uint16 binary"
+  #   puts uint16_slice
+
+  #   puts "Decoding using little endian"
+
+  #   io2 = IO::Memory.new(message)
+
+  #   # typ2 = io2.read_bytes(UInt8, FORMAT)
+  #   # categ2 = io2.read_bytes(UInt8, FORMAT)
+  #   # usec2 = io2.read_bytes(UInt64, FORMAT)
+
+  #   num1 = io2.read_bytes(UInt16, FORMAT)
+  #   num2 = io2.read_bytes(UInt16, FORMAT)
+
+  #   puts num1
+  #   puts num2
+    
+  #   # TESTING: echo
+  #   socket.send(message) # sending and receiving, all in single bytes...
+  # end
+
+  # # Handle disconnection and clean sockets
+  # socket.on_close do |_|
+  #   # sockets.delete(socket)
+  #   puts "Closing Socket: #{socket}"
+  # end
--- src/server.cr
+++ src/server.cr
@@ -17,6 +17,12 @@ get "/" do
   "Hello World! #{server_memory}"
 end
 
+# TODO: ... set up auth endpoint, SSO Google
+# and store the user info in session that can be accessed in websocket.
+# .. "/..." do
+#   # TODO: ...
+# end
+
 get "/players" do
   "players..."
 end
@@ -37,16 +43,15 @@ get "/play" do
   "playing..."
 end
 
+# TODO: secure against specific creds
+get "/admin" do
+  "server admin only..."
+end
+
 get "/testy/:name" do |env|
   name = env.params.url["name"]
   render "src/views/testy.ecr", "src/views/layouts/default.ecr"
 end
-
-# TODO: ... set up auth endpoint, SSO Google
-# and store the user info in session that can be accessed in websocket.
-# .. "/..." do
-#   # TODO: ...
-# end
 
 
 # WARNING: if crystal releases parallel update, fibers will access local data async !!!
@@ -64,64 +69,14 @@ ws "/" do |socket, env|
 
   # TODO: read from session
   # client = Client.new(:socket => socket, :session_id => session_id)
-  client = Pulse::Client.new(socket: socket, client_id: Random::Secure.hex)
+  client = Pulse::Client.new(socket: socket, client_id: Random::Secure.hex) # random id for testing
+  client.authenticate!
+  client.enter_room(maps)
+
   # clients.push(client)
 
-  # Handle incoming message and dispatch it to all connected clients
-  socket.on_message do |message|
-    # messages.push message
-    # sockets.each do |a_socket|
-    #   a_socket.send messages.to_json
-    # end
-
-    
-    puts "non binary"
-    puts message # TESTING...
-    
-    # TESTING: echo
-    socket.send(message)
-  end
-
-  socket.on_binary do |message|
-    # messages.push message
-    # sockets.each do |a_socket|
-    #   a_socket.send messages.to_json
-    # end
-
-    
-    puts "binary"
-    puts message # TESTING...
-    puts message.class # TESTING...
-
-    # Converting to message from Slice(UInt8) to Slice(UInt16)
-    uint16_slice = Slice(UInt16).new(message.to_unsafe.as(Pointer(UInt16)), message.size // 2)
-
-    puts "converted to uint16 binary"
-    puts uint16_slice
-
-    puts "Decoding using little endian"
-
-    io2 = IO::Memory.new(message)
-
-    # typ2 = io2.read_bytes(UInt8, FORMAT)
-    # categ2 = io2.read_bytes(UInt8, FORMAT)
-    # usec2 = io2.read_bytes(UInt64, FORMAT)
-
-    num1 = io2.read_bytes(UInt16, FORMAT)
-    num2 = io2.read_bytes(UInt16, FORMAT)
-
-    puts num1
-    puts num2
-    
-    # TESTING: echo
-    socket.send(message) # sending and receiving, all in single bytes...
-  end
-
-  # Handle disconnection and clean sockets
-  socket.on_close do |_|
-    # sockets.delete(socket)
-    puts "Closing Socket: #{socket}"
-  end
+rescue ex : Pulse::Unauthorized
+  # TODO: ...
 end
 
 
