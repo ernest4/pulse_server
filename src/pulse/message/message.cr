b0d67dee66634679b7f437f45381dde5dc73391f
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2020-10-23 22:31:42 +0100

    position parser progress

--- src/pulse/message/message.cr
+++ src/pulse/message/message.cr
@@ -3,54 +3,46 @@ module Pulse
   class Message
     MESSAGE_TYPE_CLASSES = {
       1 => Pulse::Message::Position,
-      # TODO: rest...
+      # TODO: attack...
+      # TODO: buff (drinking potion, spell etc)
     }
 
-    FORMAT = IO::ByteFormat::LittleEndian
+    # getter :message
 
-    def parse(message : Slice(UInt8))
-      MESSAGE_TYPE_CLASSES[get_message_type(message)].new.parse(message)
+    def initialize(message : Slice(UInt8))
+      @message = message
     end
 
-    def get_message_type(message)
-      # TODO: return type
+    def parse
+      MESSAGE_TYPE_CLASSES[class_type].new.parse(message)
+    end
+
+    def class_type
+      get_byte
+    end
+
+    def get_byte
+      io_memory_message.read_bytes(UInt8, IO::ByteFormat::LittleEndian)
+    end
+
+    def get_number
+      io_memory_message.read_bytes(UInt16, IO::ByteFormat::LittleEndian)
+    end
+
+    def get_string
+      # TODO: ...
+      # buffer = uninitialized UInt8[1024]
+      # bytes_read = io2.read_utf8(buffer.to_slice)
+      # slice = buffer.to_slice[0, bytes_read]
+      # puts slice
+      # stringy2 = String.new(slice)
+    end
+
+    def io_memory_message
+      @io_memory_message ||= IO::Memory.new(@message)
     end
   end
 end
-
-
-  # binary message handling
-
-  # FORMAT = IO::ByteFormat::LittleEndian
-
-    #   puts "binary"
-  #   puts message # TESTING...
-  #   puts message.class # TESTING...
-
-  #   # Converting to message from Slice(UInt8) to Slice(UInt16)
-  #   uint16_slice = Slice(UInt16).new(message.to_unsafe.as(Pointer(UInt16)), message.size // 2)
-
-  #   puts "converted to uint16 binary"
-  #   puts uint16_slice
-
-  #   puts "Decoding using little endian"
-
-  #   io2 = IO::Memory.new(message)
-
-  #   # typ2 = io2.read_bytes(UInt8, FORMAT)
-  #   # categ2 = io2.read_bytes(UInt8, FORMAT)
-  #   # usec2 = io2.read_bytes(UInt64, FORMAT)
-
-  #   num1 = io2.read_bytes(UInt16, FORMAT)
-  #   num2 = io2.read_bytes(UInt16, FORMAT)
-
-  #   puts num1
-  #   puts num2
-
-
-
-
-
 
 
 # ----------------------- this encoding works with any string...
@@ -73,7 +65,7 @@ end
 # buf = io.to_slice
 # puts buf
 
-# # decoding
+# # decoding -----------------------------
 
 # io2 = IO::Memory.new(buf)
 
--- src/pulse/message/position.cr
+++ src/pulse/message/position.cr
@@ -4,7 +4,9 @@ module Pulse
     class Position < Pulse::Message
       property :position_x, :position_y
 
-      def initialize(position_x : UInt16, position_y : UInt16)
+      def initialize(position_x : UInt16, position_y : UInt16, message : Slice(UInt8))
+        super(message)
+
         @position_x = position_x
         @position_y = position_y
       end
@@ -15,9 +17,14 @@ module Pulse
 
       # end
 
-      def parse(message : Slice(UInt8))
-        # TODO: extract info from message and populate instancs vars
-        # TODO: return self
+      def parse
+        # @message_type = get_byte
+        get_byte # get the type byte and move the reader pointer on to next value
+
+        @position_x = get_number
+        @position_y = get_number
+
+        self # return self
       end
     end
   end
