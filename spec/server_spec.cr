ecedc86b323ae2afdd15b875d027df7b258a6a3f
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2020-10-30 20:04:36 +0000

    adding spectator. RSpec like specs

--- spec/server_spec.cr
+++ spec/server_spec.cr
@@ -1,11 +1,18 @@
 require "./spec_helper"
 
-# TODO: migrate to RSpec style expect() https://github.com/icy-arctic-fox/spectator
+# describe "Your::Kemal::App" do
+#   # You can use get,post,put,patch,delete to call the corresponding route.
+#   it "renders /" do
+#     get "/"
+#     response.body.should contain "<title>Multiplayer Application</title>"
+#   end
+# end
 
-describe "Your::Kemal::App" do
+Spectator.describe "Pulse Server" do
   # You can use get,post,put,patch,delete to call the corresponding route.
+  before_each { get "/" }
+
   it "renders /" do
-    get "/"
-    response.body.should contain "<title>Multiplayer Application</title>"
+    expect(response.body).to contain "<title>Multiplayer Application</title>"
   end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -2,7 +2,8 @@ ENV["SECRET"] = "123"
 ENV["KEMAL_ENV"] = "test"
 
 
-require "spec"
+# require "spec"
 require "spec-kemal"
+require "spectator" # more RSpec like expectations. Preferred over "spec"
 require "../src/server"
 
