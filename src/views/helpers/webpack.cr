735e8801054be68998682d616b5b39eb301bc112
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2020-11-06 18:00:21 +0000

    bit more specific

--- src/views/helpers/webpack.cr
+++ src/views/helpers/webpack.cr
@@ -3,13 +3,13 @@ module Pulse
     extend self
 
     def stylesheet_pack_tag(name)
-      hashed_file_name = Dir["public/app/**/*.css"].first.split("/").last
+      hashed_file_name = Dir["public/app/css/*.css"].first.split("/").last
 
       "<link href=\"/app/css/#{hashed_file_name}\" rel=\"stylesheet\">"
     end
 
     def javascript_pack_tag(name)
-      hashed_file_name = Dir["public/app/**/*.js"].first.split("/").last
+      hashed_file_name = Dir["public/app/js/*.js"].first.split("/").last
 
       "<script src=\"/app/js/#{hashed_file_name}\"></script>"
     end
