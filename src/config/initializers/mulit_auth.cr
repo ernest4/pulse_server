0d036c24078e999abc3f4b9d218897cb312ed391
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2020-12-07 21:02:07 +0000

    deploy fix for heroku

--- src/config/initializers/mulit_auth.cr
+++ src/config/initializers/mulit_auth.cr
@@ -5,6 +5,7 @@ MultiAuth.config("google", ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"])
 
 def self.multi_auth(env)
   provider = env.params.url["provider"]
-  redirect_uri = "#{Kemal.config.scheme}://#{env.request.host_with_port.as(String)}/multi-auth/#{provider}/callback"
+  # redirect_uri = "#{Kemal.config.scheme}://#{env.request.host_with_port.as(String)}/multi-auth/#{provider}/callback"
+  redirect_uri = "#{ENV["SCHEME"]? || "http"}://#{env.request.host_with_port.as(String)}/multi-auth/#{provider}/callback"
   MultiAuth.make(provider, redirect_uri)
 end
